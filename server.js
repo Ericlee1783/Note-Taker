0d9df0d61169ed61924bdfa1a9ad05e485342f09
Ericlee1783 <ericlee1783@gmail.com>
2023-10-11 16:52:03 -0700

    added savepoint

--- server.js
+++ server.js
@@ -4,26 +4,32 @@ const data = require('./db/db.json')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 const { v4: uuidv4 } = require('uuid');
+
 let notes = [];
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/404.html'))
-);
+
+
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/404.html'))
+// );
 
 
 app.get('/api/notes', (req, res) => {
-  res.json(data);
+  res.json(notes);
+  console.log(notes)
 })
 
 app.post('/api/notes', (req, res) => {
@@ -35,6 +41,11 @@ app.post('/api/notes', (req, res) => {
   notes.push(newNote);
   res.json(newNote);
 });
+
+
+app.get('*', (req, res) =>
+res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
